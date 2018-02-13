297275a8d109728c0d9b468d83490df356f6c576
David M. Miller <daxdoxsi@gmail.com>
2018-02-12 19:12:01 -0600

    Update links for bootstrap 4 library

--- src/views/templates/blank.php
+++ src/views/templates/blank.php
@@ -24,8 +24,7 @@
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="/js/jquery-3.2.1.slim.min.js"></script>
-<script src="/js/popper.min.js"></script>
-<script src="/js/bootstrap.min.js"></script>
+<script src="/js/bootstrap.bundle.min.js"></script>
 <script src="/js/scripts.js"></script>
 </body>
 </html>
--- src/views/templates/default.php
+++ src/views/templates/default.php
@@ -26,8 +26,7 @@
 
     <!-- Latest compiled and minified JavaScript -->
     <script src="/js/jquery-3.2.1.slim.min.js"></script>
-    <script src="/js/popper.min.js"></script>
-    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/bootstrap.bundle.min.js"></script>
     <script src="/js/scripts.js"></script>
 </body>
 </html>
